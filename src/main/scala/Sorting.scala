84d2b8edbb6b43380f7df1bb4b318ba34ec70d59
scott <scott@paradox-box>
2014-05-31 16:46:21 +0800

    add a isSorted() method

--- src/main/scala/Sorting.scala
+++ src/main/scala/Sorting.scala
@@ -1,7 +1,14 @@
 package algo
 
 object Sorting {
-  def sort[T](lst: Seq[T], lt: (T, T) => Int): Seq[T]
-    = lst.sortWith((x,y) => lt(x,y) < 0) 
+  def sort[T](lst: Seq[T], compare: (T, T) => Int): Seq[T] = 
+    lst.sortWith((x,y) => compare(x,y) < 0) 
+
+  def isSorted[T](lst: Seq[T], compare: (T, T) => Int): Boolean =
+    lst match {
+      case Nil => true
+      case _ :: Nil => true
+      case h1 :: h2 :: tail => (compare(h1, h2) < 0) && isSorted(tail, compare)
+    }
 }
 
--- src/test/scala/SortingSuite.scala
+++ src/test/scala/SortingSuite.scala
@@ -5,7 +5,13 @@ import org.scalatest.FunSuite
 class SortingSuite extends FunSuite {
   import algo.Sorting._
 
+  private def c1 = (x: Int, y: Int) => x-y
+  private def c2 = (x: Int, y: Int) => y-x
+
+  def increase(lst: Seq[Int]) = isSorted(lst, c1)
+  def decrease(lst: Seq[Int]) = isSorted(lst, c2)
+
   test("basic sorting via lib") {  
-    assert(sort(List(1, 6, 4, 5, 3), (x: Int,y: Int) => x-y) === List(1, 3, 4, 5, 6))
+    assert(increase(sort(List(1, 6, 4, 5, 3), c1)))
   }
 }
